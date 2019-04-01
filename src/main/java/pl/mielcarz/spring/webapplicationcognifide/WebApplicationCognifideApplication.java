f403268928f4cac5d3c10517cc313ea5c8565e1c
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-04-02 00:17:43 +0200

    Initialize commit

---
+++ src/main/java/pl/mielcarz/spring/webapplicationcognifide/WebApplicationCognifideApplication.java
@@ -0,0 +1,13 @@
+package pl.mielcarz.spring.webapplicationcognifide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationCognifideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationCognifideApplication.class, args);
+    }
+
+}
